4c40d12a8885ccf650e6e852c88f9780ee8edcdf
Alan <blog@shibuya246.com>
2013-05-07 18:40:40 +0900

    language files

---
+++ hybridauth/languages/hybridauth_language.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * HYBRIDAUTH LANGUAGE
+ *
+ * PHP version 5
+ *
+ * LICENSE: Hotaru CMS is free software: you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of 
+ * the License, or (at your option) any later version. 
+ *
+ * Hotaru CMS is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE. 
+ *
+ * You should have received a copy of the GNU General Public License along 
+ * with Hotaru CMS. If not, see http://www.gnu.org/licenses/.
+ * 
+ * @category  Content Management System
+ * @package   HotaruCMS
+ * @author    author: Graeme Fulton
+ * @link      http://www.hotarucms.org/
+ */
+
+
+/* Admin Settings */
+$lang['hybridauth_admin_sidebar'] = "HybridAuth";
+$lang["hybridauth_settings_header"] = "HybridAuth Settings";
+$lang["hybridauth_settings_instructions"] = "Options for comments:";
+
+?>
